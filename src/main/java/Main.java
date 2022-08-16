50649e530e364348ef61b714321f028f79b918bf
Tekle Gebreyohannes <tgyohannes@excellerentsolutions.com>
2022-08-16 10:51:54 +0300

    Add Main class with basic setup

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import com.streams_api.Gender;
+import com.streams_api.Person;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getPeople());
+    }
+
+    private static List<Person> getPeople() {
+        return List.of(
+                new Person("James Bond", 20, Gender.MALE),
+                new Person("Alitia Smith", 33, Gender.FEMALE),
+                new Person("Helen White", 57, Gender.FEMALE),
+                new Person("Alex Boz", 14, Gender.MALE),
+                new Person("Jamie Goa", 99, Gender.MALE),
+                new Person("Anna Cook", 7, Gender.FEMALE),
+                new Person("Zelda Brown", 120, Gender.FEMALE)
+        );
+    }
+}
